3426f0bd9ff7f2d82760bad2abea1920b9bdca80
Olga Semenova <helen597597@gmail.com>
2024-07-14 02:48:24 +0500

    add csu, register models, edit models

--- electronics_shop/admin.py
+++ electronics_shop/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from electronics_shop.models import Product, Supplier
+from django.utils.html import format_html
 
 # Register your models here.
+admin.site.register(Product)
+
+
+@admin.action(description='Clear debt')
+def clear_debt(queryset):
+    queryset.update(debt=0.00)
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = (
+        'level', 'type', 'title', 'link_to_supplier', 'debt', 'created_at',)
+    list_display_links = ('title',)
+    list_filter = ('city',)
+    search_fields = ('title',)
+    actions = (clear_debt,)
+
+    def link_to_supplier(self, obj):
+        if obj.the_supplier:
+            link = f'/admin/electronics_shop/supplier/1/change/'
+            return format_html('<a href="{}">Supplier {}</a>', link, obj.the_supplier.title)
+        return ''
+
+    link_to_supplier.short_description = 'Show supplier'
--- electronics_shop/models.py
+++ electronics_shop/models.py
@@ -15,7 +15,7 @@ LEVEL_CHOICES = (
 
 # Create your models here.
 class Supplier(models.Model):
-    type = models.CharField(choices=TYPE_CHOICES)
+    type = models.CharField(max_length=13, choices=TYPE_CHOICES)
     level = models.SmallIntegerField(choices=LEVEL_CHOICES)
     title = models.CharField(max_length=150, verbose_name='Title')
     email = models.EmailField(verbose_name='E-mail')
@@ -23,9 +23,7 @@ class Supplier(models.Model):
     city = models.CharField(max_length=150, verbose_name='City')
     street = models.CharField(max_length=150, verbose_name='Street')
     house_number = models.CharField(max_length=150, verbose_name='Number of a house')
-    product = models.ManyToManyField(
-        'Product', **NULLABLE, on_delete=models.SET_NULL, verbose_name='Product')
-    supplier = models.ForeignKey(
+    the_supplier = models.ForeignKey(
         'Supplier', **NULLABLE, on_delete=models.SET_NULL, verbose_name='Supplier')
     debt = models.DecimalField(max_digits=11, decimal_places=2, default=0.00, verbose_name='Debt')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation time')
@@ -43,6 +41,8 @@ class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name='Title')
     model = models.CharField(max_length=150, verbose_name='Model')
     date = models.DateField(default='', **NULLABLE, verbose_name='Release date')
+    supplier = models.ForeignKey(
+        'Supplier', **NULLABLE, on_delete=models.CASCADE, verbose_name='Supplier')
 
     def __str__(self):
         return f'{self.title} {self.model}'
