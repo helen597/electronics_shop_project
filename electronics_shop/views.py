69254c1a74d9f6931007f2054dace0e29c49d2b3
Olga Semenova <helen597597@gmail.com>
2024-07-15 00:28:39 +0500

    add Supplier views

--- electronics_shop/views.py
+++ electronics_shop/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from electronics_shop.models import Supplier, Product
+from electronics_shop.serializers import SupplierSerializer, ProductSerializer
+from rest_framework.filters import SearchFilter
+
 
 # Create your views here.
+class SupplierListAPIView(ListAPIView):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objecs.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['country']
+
+
+class SupplierRetrieveAPIView(RetrieveAPIView):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objecs.all()
+
+
+class SupplierCreateAPIView(CreateAPIView):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objecs.all()
+
+
+class SupplierUpdateAPIView(UpdateAPIView):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objecs.all()
+
+
+class SupplierDestroyAPIView(DestroyAPIView):
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objecs.all()
+
+
+class ProductListAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
