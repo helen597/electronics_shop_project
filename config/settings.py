882f1eaad27eadc144900a6b45c82a6d435ed74b
Olga Semenova <helen597597@gmail.com>
2024-07-19 23:25:43 +0500

    fix bug

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"), override=True)
 SECRET_KEY = "django-insecure-il-0kk-)+($w%ma3*+u-kk+ejvfoohxsubjf*z2g*(v(felzq1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == True
+DEBUG = os.getenv("DEBUG") == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
